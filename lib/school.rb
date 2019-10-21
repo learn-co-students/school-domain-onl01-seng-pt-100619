24c51f6433083c0bf61b0d92e817f4d58813b2d0
Jeff Swisher <jefftswisher@gmail.com>
2019-10-21 01:46:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School 
+ attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+ 
+  def add_student(name, grade)
+      @roster[grade] ||= []
+      @roster[grade] << name
+  end 
+
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+      sorted = Hash[@roster.sort_by {|n,g| n }]
+      sorted.keys.each do |n,g|
+        sorted[n].sort!
+      end
+      sorted
+    end
+end

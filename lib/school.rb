f0df643aeaf89b779a717e3403be8ba66a353f77
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-29 19:06:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster, :name
+  
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+  
+  def add_student(name,grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+    # binding.pry
+  end
+  
+  def sort
+    @roster.each do |grade,name|
+      name.sort!
+    end
+  end
+  
+end
+
+

bc69b5c51960c23158688f212d28d8b40c04c9fb
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-06 16:11:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School 
+  attr_accessor :student_name, :grade
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def school_name(name)
+    @name = name 
+  end 
+  
+   #create the new key and point it to an empty array. Then we can push the new value into that array.
+    # hash["new_key"] = []
+    # hash["new_key"] << "new_value_for_value_array"
+ 
+    # hash
+    # => {"new_key"=>["new_value_for_value_array"]} 
+  
+  def add_student(student_name, grade)
+    @student_name = student_name 
+    @grade = grade
+    
+    if @roster.has_key?(grade)
+      @roster[grade] = []
+      @roster[grade] << value
+    else 
+      grade = []
+      @roster[grade] 
+      
+  end 
+end
+ end 
